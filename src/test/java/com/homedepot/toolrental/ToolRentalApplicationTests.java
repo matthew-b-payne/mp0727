aefc52eb0b0570877c1d1ee7414d6cb0125e3834
matthew <matthew.b.payne@gmail.com>
2024-07-28 16:51:02 -0400

    mock toolRepository so it does not load mongo on contextLoads

--- src/test/java/com/homedepot/toolrental/ToolRentalApplicationTests.java
+++ src/test/java/com/homedepot/toolrental/ToolRentalApplicationTests.java
@@ -1,13 +1,18 @@
 package com.homedepot.toolrental;
 
+import com.homedepot.toolrental.repository.ToolRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ToolRentalApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private ToolRepository toolRepository;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
